c3491ee034741247d25115acac1330860f8ec569
David Forero <104437024+DavidForero120@users.noreply.github.com>
2023-01-25 15:23:16 -0500

    .

--- nodeCrud/src/controllers/userController.js
+++ nodeCrud/src/controllers/userController.js
@@ -6,11 +6,12 @@ const bcrypt = require('bcrypt');
 controller.newUser = async (req, res)=>{
     const nombreUsuario= req.body;
     const nombre= req.body;
+    const correo = req.body;
     const rol= req.body;
-    const password = req.body;
-    const passEncrypt = await bcrypt.hash({password}, 8);
+    const {password} = req.body;
+    const passEncrypt = await bcrypt.hash(password, 8);
     req.getConnection((err, conn)=>{
-        conn.query('INSERT INTO usuario set ?', [nombreUsuario,nombre,rol, passEncrypt ], (err, rows)=>{
+        conn.query('INSERT INTO usuario set ?', [nombreUsuario,nombre,correo, rol, passEncrypt ], (err, rows)=>{
                if(err){
                 console.log('no se pudo registrar')
                }else{
